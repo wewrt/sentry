0b3d736dfe138759ba34d1d18642c82a534d9bbe
Marcos Gaeta <marcos.gaeta@sentry.io>
2020-11-05 17:16:27 -0800

    Add form fields

--- src/sentry/integrations/jira/notify_action.py
+++ src/sentry/integrations/jira/notify_action.py
@@ -10,7 +10,17 @@ logger = logging.getLogger("sentry.rules")
 
 
 class JiraNotifyServiceForm(forms.Form):
-    # TODO 1.0 Add form fields.
+    jira_integration = forms.ChoiceField(choices=(), widget=forms.Select())
+    assignee = forms.ChoiceField(choices=(), widget=forms.Select())
+    components = forms.ChoiceField(choices=(), widget=forms.Select())
+    description = forms.ChoiceField(choices=(), widget=forms.Select())
+    duedate = forms.ChoiceField(choices=(), widget=forms.Select())
+    fixVersions = forms.ChoiceField(choices=(), widget=forms.Select())
+    issuetype = forms.ChoiceField(choices=(), widget=forms.Select())
+    labels = forms.ChoiceField(choices=(), widget=forms.Select())
+    priority = forms.ChoiceField(choices=(), widget=forms.Select())
+    project = forms.ChoiceField(choices=(), widget=forms.Select())
+    reporter = forms.ChoiceField(choices=(), widget=forms.Select())
 
     def __init__(self, *args, **kwargs):
         super(JiraNotifyServiceForm, self).__init__(*args, **kwargs)
@@ -21,10 +31,19 @@ class JiraNotifyServiceForm(forms.Form):
 
 class JiraCreateTicketAction(IntegrationEventAction):
     form_cls = JiraNotifyServiceForm
-    label = u"TODO Create a {name} Jira ticket"
+    label = u"""Create a Jira ticket in the {jira_integration} account
+    and {project} project
+    of type {issuetype}
+    with components {components}
+    and due date {duedate}
+    with fixVersions {fixVersions}
+    assigned to {assignee}
+    reported by {reporter}
+    label {labels}
+    priority {priority}"""
     prompt = "Create a Jira ticket"
     provider = "jira"
-    integration_key = "jira_project"
+    integration_key = "jira_integration"
 
     def __init__(self, *args, **kwargs):
         super(JiraCreateTicketAction, self).__init__(*args, **kwargs)
